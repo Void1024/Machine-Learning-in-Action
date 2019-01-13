e10a77c7906afd706225b46fc4f0ad15e28ae95e
Yilin Zhang <2322162863@qq.com>
2019-01-13 16:55:02 +0800

    add naive bayes algorithm

---
+++ src/test/test_bayes.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('..')
+
+import algorithm.naive_bayes as Bayes
+from loader.load_mnist import load_mnist
+
+def test() :
+    path = "../../dataset/mnist"
+    dataSet,labels = load_mnist(path,'train')
+    testX,testY = load_mnist(path,'t10k')
+    Bayes.naive_bayes_train(dataSet,labels)
+    check = 0
+    total = 0
+    for i in range(len(testX)):
+        predict = Bayes.classify(testX[i])
+        if predict == testY[i]:
+            check += 1
+        total += 1
+    print("total:%d time(s),hit:%d time(s),accuracy:%f." % (total,check,check/float(total)))
+
+if __name__ == '__main__':
+    test()
